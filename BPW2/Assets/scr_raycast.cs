25f275e79ffccd4446a7781bf973c4f923a6d8f9
Goth-Kath <katja.persoons@student.hku.nl>
2018-06-11 00:13:23 +0200

    first try
    shiet

---
+++ BPW2/Assets/scr_raycast.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_raycast : MonoBehaviour
+{
+
+    public float rayDistance;
+    private Behaviour halolight;
+    
+    // Use this for initialization
+    void Start()
+    {
+        Behaviour halolight = (Behaviour)GetComponent("Halo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, rayDistance))
+        {
+            if (hit.transform.tag == "textitem")
+            {
+               
+                halolight.enabled = true;
+            }
+            if (hit.transform.tag == "bleh")
+            {
+                Destroy(hit.transform.gameObject);
+            }
+
+        }
+    }
+}
+
