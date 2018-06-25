95a9051562e94677ac39fda421ffb268187a00d4
Goth-Kath <katja.persoons@student.hku.nl>
2018-06-25 12:42:11 +0200

    wolla

--- BPW2/Assets/scrip_text.cs
+++ BPW2/Assets/scrip_text.cs
@@ -5,55 +5,32 @@ using UnityEngine.UI;
 
 public class scrip_text : MonoBehaviour {
 
-    public Text text;
+
     public string[] actualtext;
     private int nummer;
-    public int endtext;
-    public GameObject canvas;
-    private bool RayBool;
 
 
     // Use this for initialization
     void Start()
     {
-        text.text = actualtext[nummer];
+
     }
 
-    void HitByRay()
+    public void Reset()
     {
-
-        if (RayBool == false)
-        {
-            RayBool = true;
-        }
-        if (RayBool == true)
-        {
-
-            Debug.Log("I was hit by a ray");
-            RayBool = false;
-        }
-
+        nummer = 0;
     }
 
-    // Update is called once per frame
-    void Update() {
-        if (Input.GetKeyDown("e") && RayBool == true)
-            {
-            print(nummer);
-
-            
-            nummer++;
-            if (nummer >= endtext)
-            {
-                canvas.SetActive(false);
-                nummer = 0;
-                text.text = actualtext[nummer];
-            }
-            else
-            {
-                text.text = actualtext[nummer];
-            }
+    public string GetNextText()
+    {
+        string result = "";
+        if(nummer < actualtext.Length)
+        {
+            result = actualtext[nummer];
         }
 
+        nummer++;
+        return result;
     }
+
 }
--- BPW2/Assets/scripts/scr_raycast.cs
+++ BPW2/Assets/scripts/scr_raycast.cs
@@ -1,25 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class scr_raycast : MonoBehaviour
 {
 
     public float rayDistance;
-    [SerializeField]
-    public Behaviour haloplant;
-    public Behaviour groundpillow;
-    public Behaviour bedpillow1;
-    public Behaviour bedpillow2;
-    public Behaviour dresserknob;
-    public Behaviour dresserbook;
-    public Behaviour groundbook;
-    public Behaviour standlamp1;
-    public Behaviour standlamp2;
-    public Behaviour posters;
-    public Behaviour closetdoor1;
-    public Behaviour closetdoor2;
-    public Behaviour bedpainting;
+    public Behaviour currentHalo;
+    public scrip_text currentText;
+    public Text textComponent; 
     public GameObject canvas;
    
     public GameObject closetclosed;
@@ -40,147 +29,66 @@ public class scr_raycast : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit, rayDistance))
         {
-            hit.transform.SendMessage("HitByRay");
-            haloplant.enabled = false;
-            groundpillow.enabled = false;
-            bedpillow1.enabled = false;
-            bedpillow2.enabled = false;
-            dresserknob.enabled = false;
-            dresserbook.enabled = false;
-            groundbook.enabled = false;
-            standlamp1.enabled = false;
-            standlamp2.enabled = false;
-            posters.enabled = false;
-            closetdoor1.enabled = false;
-            closetdoor2.enabled = false;
-            bedpainting.enabled = false;
-
-
-
-
-            if (hit.transform.tag == "a")
+            //Halo Behaviour
+            if (currentHalo != null)
             {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    canvas.SetActive(true);
-                }
-                    haloplant.enabled = true;
+                currentHalo.enabled = false;
+            }
+            Behaviour haloBehaviour = (Behaviour)hit.collider.GetComponent("Halo");
+
+            if (haloBehaviour != null)
+            {
+                haloBehaviour.enabled = true;
+                currentHalo = haloBehaviour;
             }
 
-            if (hit.transform.tag == "groundpillow")
+            //Text laten zien
+            if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.R))
             {
-                if (Input.GetMouseButtonDown(0))
+                currentText = hit.collider.GetComponent<scrip_text>();
+                if (currentText != null)
                 {
                     canvas.SetActive(true);
-                }
-                groundpillow.enabled = true;
-            }
 
-            if (hit.transform.tag == "bedpillow1")
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    canvas.SetActive(true);
+                    currentText.Reset();
+                    ShowText();
                 }
-                bedpillow1.enabled = true;
-            }
 
-            if (hit.transform.tag == "bedpillow2")
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    canvas.SetActive(true);
-                }
-                bedpillow2.enabled = true;
             }
+        }
+        else
+        {
+            DontShowCanvasAndResetText();
+        }
+        if (Input.GetKeyDown(KeyCode.E) && currentText != null)
+        {
+            ShowText();
+        }
 
-            if (hit.transform.tag == "dresserknob")
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    
-                    dresseropen.SetActive(true);
-                    dresserclosed.SetActive(false);
-                    keybool = true;
-                }
-                dresserknob.enabled = true;
-            }
 
-            if (hit.transform.tag == "dresserbook")
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    canvas.SetActive(true);
-                }
-                dresserbook.enabled = true;
-            }
-            if (hit.transform.tag == "groundbook")
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    canvas.SetActive(true);
-                }
-                groundbook.enabled = true;
-            }
-            if (hit.transform.tag == "standlamp1")
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    canvas.SetActive(true);
-                }
-                standlamp1.enabled = true;
-            }
-            if (hit.transform.tag == "standlamp2")
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    canvas.SetActive(true);
-                }
-                standlamp2.enabled = true;
-            }
-            if (hit.transform.tag == "posters")
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    canvas.SetActive(true);
-                }
-                posters.enabled = true;
-            }
-            if (hit.transform.tag == "closetdoor1")
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    if(keybool == true)
-                    {
-                        closetclosed.SetActive(false);
-                        closetopen.SetActive(true);
-                    }
-                    canvas.SetActive(true);
-                }
-                closetdoor1.enabled = true;
-            }
-            if (hit.transform.tag == "closetdoor2")
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    if (keybool == true)
-                    {
-                        Destroy(closetclosed);
-                        closetopen.SetActive(true);
-                    }
-                    canvas.SetActive(true);
-                }
-                closetdoor2.enabled = true;
-            }
-            if (hit.transform.tag == "bedpainting")
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    canvas.SetActive(true);
-                }
-                bedpainting.enabled = true;
-            }
-           
+    }
+
+    void DontShowCanvasAndResetText()
+    {
+        if(currentText != null)
+        {
+            currentText.Reset();
+        }
+        
+        currentText = null;
+        canvas.SetActive(false);
+    }
+
+    void ShowText()
+    {
+        string text = currentText.GetNextText();
+        if (text != "")
+        {
+            textComponent.text = text;
+        }
+        else
+        {
+            DontShowCanvasAndResetText();
         }
     }
 }
